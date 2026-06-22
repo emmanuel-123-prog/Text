6b2258666bb79cb3b2afc518ab59efe8f961b87e
eT <davidosagie2024@gmail.com>
2026-06-22 14:44:21 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-  apiKey: "AIzaSyD_TmjkWu9a7TUBPgEbOKUw5GS_dwpsrMU",
-  authDomain: "text-cb2a5.firebaseapp.com",
-  databaseURL: "https://text-cb2a5-default-rtdb.firebaseio.com",
-  projectId: "text-cb2a5",
-  storageBucket: "text-cb2a5.firebasestorage.app",
-  messagingSenderId: "521856539812",
-  appId: "1:521856539812:web:8168887697e122367f9a31",
-  measurementId: "G-7QRZS4CSNT"
